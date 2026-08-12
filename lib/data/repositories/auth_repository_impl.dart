662c49735ed061a61a87f213404818c275586046
Dan-Eb <ebjdan@gmail.com>
2026-08-12 23:10:41 +0100

    saving progress

--- lib/data/repositories/auth_repository_impl.dart
+++
@@ -1,8 +0,0 @@
-import 'package:social_and_recommendation_system/data/models/user_model.dart';
-
-abstract class AuthRepository {
-Future<UserModel?> login(String email, String password);
-Future<UserModel?> signup(String name, String email, String password);
-Future<void> logout();
-
-}
