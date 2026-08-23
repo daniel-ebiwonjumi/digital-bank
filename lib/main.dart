e38b34725a60ecd66ab46e9f67e985bcf0386761
Dan-Eb <ebjdan@gmail.com>
2026-08-23 23:47:28 +0100

    saving progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:digital_bank/data/repositories/auth_repostory.dart';
 import 'package:digital_bank/data/services/auth_services/auth_service.dart';
 import 'package:digital_bank/data/services/auth_services/auth_token_storage.dart';
+import 'package:digital_bank/ui/auth/auth_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:digital_bank/app/app.dart';
 
@@ -11,8 +12,9 @@ void main(){
   final api = Api(authTokenStorage);
   final authService  = AuthService(api);
   final authRepository = AuthRepository(authService: authService, authTokenStorage: authTokenStorage);
+  final authViewModel = AuthViewModel(authRepository);
 
-  runApp(const DigitalBank(
+  runApp( DigitalBank(
     authViewModel: authViewModel,
   ));
 }
