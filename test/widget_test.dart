ad494aadbbf8da354522a2d4829bd416a03e9c5a
Dan-Eb <ebjdan@gmail.com>
2026-08-24 00:12:08 +0100

    saving progress

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:digital_bank/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const FullApp());
+    await tester.pumpWidget(const DigitalBank());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
