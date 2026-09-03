8499efa5462a09a9b641309bf68b952b0f8fe4eb
Daniel Ebiwonjumi <ebjdan@gmail.com>
2026-09-03 10:46:33 +0100

    Update home_service.dart

--- lib/data/services/home_service/home_service.dart
+++ lib/data/services/home_service/home_service.dart
@@ -2,16 +2,8 @@ import 'package:digital_bank/data/services/api_config.dart';
 import 'package:dio/dio.dart';
 
 class HomeService {
+late final DioConfig dioConfig;
   Future<Response> getHomeData() async {
-    return await dio.get(ApiConfig.home);
+    return await dioConfig.dio.get(ApiConfig.home);
   }
 }
-
-Dio dio = Dio(
-  BaseOptions(
-    baseUrl: '', //TODO: put the proper url here later
-    connectTimeout: Duration(seconds: 15),
-    receiveTimeout: Duration(seconds: 15),
-    headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
-  ),
-);
