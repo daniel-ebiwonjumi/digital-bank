0aeeb4d3a3f92a496473103985eec8a49c314f6c
Dan-Eb <ebjdan@gmail.com>
2026-09-05 09:12:46 +0100

    saving progress

--- project/src/auth/auth.controller.ts
+++ project/src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  @HttpCode(HttpStatus.OK)
-  @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signIn(signInDto.username, signInDto.password);
-  }
+    constructor(private readonly authservice: AuthService) { }
+    
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    login(@Body() LoginDto: Record<String, any>) {
+        return this.authservice.login(LoginDto.mobileNumber, LoginDto.pass)
+}
 }
