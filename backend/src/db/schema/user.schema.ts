dcadebed4bbe6ada937a517abbb3a94192ad9d88
Daniel Ebiwonjumi <ebjdan@gmail.com>
2026-09-24 08:35:40 +0100

    Update user.schema.ts

---
+++ backend/src/db/schema/user.schema.ts
@@ -0,0 +1,9 @@
+import { pgTable, varchar, uuid } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('usera', {
+id: uuid('id'). defaultRandom(). primaryKey(),
+mobileNumber: varchar('mobile_number', {length: 10}).notNull().unique(),
+passwordHash: varchar('password_hash', {length: 250}).notNull(),
+email: varchar('email', {length: 250}).n
+unique()
+});
