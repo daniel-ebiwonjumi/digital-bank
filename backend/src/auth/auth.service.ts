9aaa6dfe7638ffe3765fad4da04dba85705e6323
Dan-Eb <ebjdan@gmail.com>
2026-09-25 10:26:47 +0100

    saving progress

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service.js';
+import { users } from '../db/schema/user.schema.js';
 
-argon2
+
 
 @Injectable()
-class 
+class AuthService {
+  constructor( private readonly usersService: UsersService ) { }
+  
+  async validateUser(mobileNumber: string, password: string) {
+    const userExists = await this.usersService.findByMobileNumber(mobileNumber);
+    if (!userExists) { return null; }
+    const passwordHashMatches = await argon2.verify(users.passwordHash, password);
+    if (!passwordHashMatches) { return null; }
+
+    const { passwordHash,...safeUser } = userExists;
+    return safeUser;
+  }
+}
